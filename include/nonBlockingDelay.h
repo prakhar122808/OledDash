5dd6d523a637d0b0c2dd914d1f12eaba58f4b42a
prakhar122808 <prakharshi2808@gmail.com>
2026-07-10 19:20:10 +0530

    Added non blocking delay header

---
+++ include/nonBlockingDelay.h
@@ -0,0 +1,62 @@
+#ifndef NON_BLOCKING_DELAY_H
+#define NON_BLOCKING_DELAY_H
+
+#include <Arduino.h>
+
+class NonBlockingDelay
+{
+
+private:
+    unsigned long previousMillis;
+    unsigned long interval;
+    bool isActive;
+
+public:
+    NonBlockingDelay(unsigned long ms = 1000)
+    {
+        interval = ms;
+        previousMillis = 0;
+        isActive = false;
+    }
+
+    void setInterval(unsigned long ms)
+    {
+        interval = ms;
+    }
+
+    void start()
+    {
+        previousMillis = millis();
+        isActive = true;
+    }
+
+    void stop()
+    {
+        isActive = false;
+    }
+
+    bool isElapsed()
+    {
+        if (!isActive)
+            return false;
+        if (millis() - previousMillis >= interval)
+        {
+            isActive = false;
+            return true;
+        }
+        return false;
+    }
+
+    bool check()
+    {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval)
+        {
+            previousMillis = currentMillis;
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif
