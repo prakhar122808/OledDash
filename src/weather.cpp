770b1ec6ddc6dbc077381bcf1820270d0337f9f1
prakhar122808 <prakharshi2808@gmail.com>
2026-07-12 22:43:23 +0530

    Revamped the UI, temporarily removed displaying the 'feels like' section

--- src/weather.cpp
+++ src/weather.cpp
@@ -1,24 +1,25 @@
 #include <ArduinoJson.h>
+#include <cstring>
 #include <HTTPClient.h>
 
 #include "header.h"
 
-Temperatures getTemps()
+Weather getWeather()
 {
     // Verify WiFi connection
     if (WiFi.status() != WL_CONNECTED)
     {
         Serial.println("WiFi not connected, skipping fetch.");
-        return cachedTemps;
+        return cachedWeather;
     }
 
     // Check time since laast update
     if (millis() - lastFetchTime < fetchInterval && lastFetchTime != 0)
     {
-        return cachedTemps;
+        return cachedWeather;
     }
 
-    Temperatures temps = {-1, -1};
+    Weather weather = {-1, -1, -1, " ", " "};
 
     HTTPClient http;
     String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + String(lat, 7) + "&lon=" + String(lon, 7) + "&appid=" + API_KEY + "&units=metric";
@@ -37,10 +38,17 @@ Temperatures getTemps()
             Serial.print("Failed to parse JSON data.");
             Serial.println(error.c_str());
             http.end();
-            return temps;
+            return weather;
         }
-        temps.actualTemp = doc["main"]["temp"];
-        temps.feelsLikeTemp = doc["main"]["feels_like"];
+        weather.actualTemp = doc["main"]["temp"];
+        weather.feelsLikeTemp = doc["main"]["feels_like"];
+        weather.humidity = doc["main"]["humidity"];
+
+        // Null pointer guard
+        const char *description = doc["weather"][0]["description"] | "";
+        const char *icon = doc["weather"][0]["icon"] | "";
+        std::snprintf(weather.weather, sizeof(weather.weather), "%s", description);
+        std::snprintf(weather.weatherIcon, sizeof(weather.weatherIcon), "%s", icon);
     }
     else
     {
@@ -48,54 +56,75 @@ Temperatures getTemps()
     }
     http.end();
 
-    cachedTemps = temps;
+    cachedWeather = weather;
     lastFetchTime = millis();
-    return cachedTemps;
+    return cachedWeather;
 }
 
 void displayWeather()
 {
+    selectedFeature = 0;
     // Plan to add option to use external hardware to get humidity and tempertaure
     // and defaulting to external API when external hardware isn't present
 
     u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_t0_11b_tf);
-    Temperatures temperatures = getTemps();
+    Weather weather = getWeather();
 
     // -------------------------Actual temperature-------------------------
-    const char *actualTempText = "Temperature: ";
-    int actualTempPos = u8g2.getStrWidth(actualTempText);
-    char actualTempbuffer[7];
-    snprintf(actualTempbuffer, sizeof(actualTempbuffer), "%.2f", temperatures.actualTemp);
-    int actualunitSymbolPos = actualTempPos + u8g2.getStrWidth(actualTempbuffer) + 2;
-
-    u8g2.setCursor(0, positions[0]);
+    u8g2.setFont(u8g2_font_6x10_tf);
+    const char *actualTempText = "Temperature:";
+    int actualTempTextPos = u8g2.getStrWidth(actualTempText);
+    u8g2.setCursor((128 - actualTempTextPos) / 2, 7);
     u8g2.print(actualTempText);
-    u8g2.setCursor(actualTempPos, positions[0]);
-    u8g2.print(actualTempbuffer);
+
+    u8g2.setFont(u8g2_font_10x20_tf);
+    char actualTemp[7];
+    snprintf(actualTemp, sizeof(actualTemp), "%.2f", weather.actualTemp);
+    int actualTempPos = u8g2.getStrWidth(actualTemp);
+    int actualUnitSymbolPos = (128 - actualTempPos) / 2 + actualTempPos;
+    u8g2.setCursor((128 - actualTempPos - 12) / 2, positions[1]);
+    u8g2.print(actualTemp);
 
     // Unit symbol
-    u8g2.drawCircle(actualunitSymbolPos, positions[0] - 7, 1);
-    u8g2.drawStr(actualunitSymbolPos + 3, positions[0], "C");
+    u8g2.drawCircle(actualUnitSymbolPos - 3, positions[0] + 3, 1);
+    u8g2.setCursor(actualUnitSymbolPos, positions[1]);
+    u8g2.print("C");
 
     // ------------------------Feels like temperature------------------------
-    const char *feelsLikeTempText = "Feels like: ";
-    int feelsLikeTempPos = u8g2.getStrWidth(feelsLikeTempText);
-    char feelsLikeTempbuffer[7];
-    snprintf(feelsLikeTempbuffer, sizeof(feelsLikeTempbuffer), "%.2f", temperatures.feelsLikeTemp);
-    int feelsLikeUnitSymbolPos = feelsLikeTempPos + u8g2.getStrWidth(feelsLikeTempbuffer) + 2;
+    // const char *feelsLikeTempText = "Feels like: ";
+    // int feelsLikeTempPos = u8g2.getStrWidth(feelsLikeTempText);
+    // char feelsLikeTempbuffer[7];
+    // snprintf(feelsLikeTempbuffer, sizeof(feelsLikeTempbuffer), "%.2f", weather.feelsLikeTemp);
+    // int feelsLikeUnitSymbolPos = feelsLikeTempPos + u8g2.getStrWidth(feelsLikeTempbuffer) + 2;
 
-    u8g2.setCursor(0, positions[1]);
-    u8g2.print(feelsLikeTempText);
-    u8g2.setCursor(feelsLikeTempPos, positions[1]);
-    u8g2.print(feelsLikeTempbuffer);
+    // u8g2.setCursor(0, positions[1]);
+    // u8g2.print(feelsLikeTempText);
+    // u8g2.setCursor(feelsLikeTempPos, positions[1]);
+    // u8g2.print(feelsLikeTempbuffer);
 
-    // Unit symbol
-    u8g2.drawCircle(feelsLikeUnitSymbolPos, positions[1] - 7, 1);
-    u8g2.drawStr(feelsLikeUnitSymbolPos + 3, positions[1], "C");
+    // // Unit symbol
+    // u8g2.drawCircle(feelsLikeUnitSymbolPos, positions[1] - 7, 1);
+    // u8g2.drawStr(feelsLikeUnitSymbolPos + 3, positions[1], "C");
 
-    u8g2.setCursor(0, positions[2]);
-    u8g2.print("Select to go back");
+    // // Divider line
+    // u8g2.drawLine(0, positions[2], 127, positions[2]);
+
+    // // Indicator showing selected page in app
+    // int x = 50;
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     if (i == selectedFeature)
+    //     {
+    //         u8g2.drawDisc(x, 56, 2);
+    //     }
+    //     else
+    //     {
+    //         u8g2.drawCircle(x, 56, 2);
+    //     }
+    //     x += 9;
+    // }
+    displayDown();
+    displayUp();
     if (!digitalRead(selectButton) && !isSelectDebouncing)
     {
         displayPage = 1;
