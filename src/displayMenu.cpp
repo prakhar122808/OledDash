e7226956e2860feab347b0fb7e40590efe9f81cb
prakhar122808 <prakharshi2808@gmail.com>
2026-07-13 02:15:25 +0530

    Added prev button effect and changed range of 'selection' to be consistent with the rest of the files

--- src/displayMenu.cpp
+++ src/displayMenu.cpp
@@ -4,18 +4,16 @@ void displayStartingMenu()
 {
     u8g2.clearBuffer();
 
-    if (!digitalRead(upButton) && !isUpDebouncing)
-    {
-        selection = (selection + 1) % 2;
-    }
+    // Application labels
+    const char *appNames[] = {"Clock", "Weather"};
+
+    // Number of apps
+    const int numApps = sizeof(appNames) / sizeof(appNames[0]);
+
     // 2x2 grid placement positions
     int colX[2] = {20, 84};
     int rowY[2] = {25, 55};
 
-    // Application labels
-    const char *appNames[] = {"Clock", "Weather"};
-
-    const int numApps = sizeof(appNames) / sizeof(appNames[0]);
     for (int i = 0; i < numApps; i++)
     {
         int col = i % 2;
@@ -23,8 +21,9 @@ void displayStartingMenu()
         int x = colX[col];
         int y = rowY[row];
 
+        int j = i + 1;
         // Draw frame around selected app
-        if (i == selection)
+        if (j == selection)
         {
             u8g2.drawRFrame(x - 14, y - 20, 52, 30, 4);
         }
@@ -32,12 +31,12 @@ void displayStartingMenu()
         // --- Icons ---
         const int appClock = 69;
         const int weatherSunBehindClouds = 65;
-        if (i == 0)
+        if (j == 1)
         {
             u8g2.setFont(u8g2_font_open_iconic_app_2x_t);
             u8g2.drawGlyph(x + 4, y - 4, appClock);
         }
-        else if (i == 1)
+        else if (j == 2)
         {
             u8g2.setFont(u8g2_font_open_iconic_weather_2x_t);
             u8g2.drawGlyph(x + 4, y - 4, weatherSunBehindClouds);
@@ -52,11 +51,25 @@ void displayStartingMenu()
         u8g2.drawStr(textX, y + 8, appNames[i]);
     }
 
+    if (!digitalRead(nextButton) && !isNextDebouncing)
+    {
+        if (selection != numApps)
+            selection++;
+        else
+            selection = 1;
+    }
+    if (!digitalRead(prevButton) && !isPrevDebouncing)
+    {
+        if (selection != 1)
+            selection--;
+        else
+            selection = numApps;
+    }
     if (!digitalRead(selectButton) && !isSelectDebouncing)
     {
         switch (selection)
         {
-        case 0:
+        case 1:
         {
             displayPage = 2;
             u8g2.setFont(u8g2_font_5x7_t_cyrillic);
@@ -64,7 +77,7 @@ void displayStartingMenu()
             u8g2.print("Loading...");
             break;
         }
-        case 1:
+        case 2:
         {
             displayPage = 3;
             u8g2.setFont(u8g2_font_5x7_t_cyrillic);
@@ -74,7 +87,9 @@ void displayStartingMenu()
         }
         }
     }
-    upDelay();
+
+    nextDelay();
+    prevDelay();
     selectDelay();
     u8g2.sendBuffer();
 }
@@ -93,15 +108,4 @@ void display(int displayPage)
         displayWeather();
         break;
     }
-}
-
-void displayUp()
-{
-    u8g2.setFont(u8g2_font_cu12_t_symbols);
-    u8g2.drawGlyph(120, 12, 0x2191); // Up arrow
-}
-void displayDown()
-{
-    u8g2.setFont(u8g2_font_cu12_t_symbols);
-    u8g2.drawGlyph(120, 64, 0x2193); // Down arrow
 }
