7ab6272c3d178826b8654f29ce493c765faa045b
prakhar122808 <prakharshi2808@gmail.com>
2026-07-13 11:39:45 +0530

    Fixed alignment of text

--- src/weather.cpp
+++ src/weather.cpp
@@ -69,41 +69,56 @@ void currentTemperatures()
 
     // -------------------------Actual temperature-------------------------
     u8g2.setFont(u8g2_font_6x10_tf);
-    const char *actualTempText = "Temperature:";
+    const char *actualTempText = "Temperature";
     int actualTempTextPos = u8g2.getStrWidth(actualTempText);
     u8g2.setCursor((128 - actualTempTextPos) / 2, 7);
     u8g2.print(actualTempText);
 
+    int offsetDueToUnit = 20;
     u8g2.setFont(u8g2_font_fub17_tf);
     char actualTemp[7];
     snprintf(actualTemp, sizeof(actualTemp), "%.2f", weather.actualTemp);
     int actualTempPos = u8g2.getStrWidth(actualTemp);
     int actualUnitSymbolPos = (128 - actualTempPos) / 2 + actualTempPos;
-    u8g2.setCursor((128 - actualTempPos - 12) / 2, positions[1]);
+    u8g2.setCursor((128 - actualTempPos - offsetDueToUnit) / 2, positions[1]);
     u8g2.print(actualTemp);
 
     // Unit symbol
-    u8g2.drawCircle(actualUnitSymbolPos - 2, positions[0] + 3, 2);
-    u8g2.setCursor(actualUnitSymbolPos + 1, positions[1]);
+    u8g2.drawCircle(actualUnitSymbolPos - (offsetDueToUnit - 13), positions[0], 2);
+    u8g2.setCursor(actualUnitSymbolPos - (offsetDueToUnit - 16), positions[1]);
     u8g2.print("C");
 
     // ------------------------Feels like temperature------------------------
     u8g2.setFont(u8g2_font_6x10_tf);
-    const char *feelsLikeTempText = "Feels like: ";
-    int feelsLikeTempPos = u8g2.getStrWidth(feelsLikeTempText);
-    char feelsLikeTempbuffer[7];
-    snprintf(feelsLikeTempbuffer, sizeof(feelsLikeTempbuffer), "%.2f", weather.feelsLikeTemp);
-    int feelsLikeUnitSymbolPos = feelsLikeTempPos + u8g2.getStrWidth(feelsLikeTempbuffer) + 2;
-    u8g2.setCursor(0, positions[2]);
-    u8g2.print(feelsLikeTempText);
+    int feelsLikeTempTextPos = u8g2.getStrWidth("Feels like ");
+
+    char feelsLikeTemp[7];
+    snprintf(feelsLikeTemp, sizeof(feelsLikeTemp), "%.2f", weather.feelsLikeTemp);
 
     u8g2.setFont(u8g2_font_6x12_tf);
-    u8g2.setCursor(feelsLikeTempPos, positions[2]);
-    u8g2.print(feelsLikeTempbuffer);
+    int feelsLikeTempPos = u8g2.getStrWidth(feelsLikeTemp);
 
-    // Unit symbol
-    u8g2.drawCircle(feelsLikeUnitSymbolPos + 1, positions[2] - 7, 1);
-    u8g2.drawStr(feelsLikeUnitSymbolPos + 4, positions[2], "C");
+    // Width of the "°C" symbol
+    int feelsLikeUnitSymbolPos = (2 + 1 + 2) + u8g2.getStrWidth("C");
+
+    // Total width of the entire combined line
+    int totalWidth = feelsLikeTempTextPos + feelsLikeTempPos + feelsLikeUnitSymbolPos;
+
+    int startPos = (128 - totalWidth) / 2;
+
+    u8g2.setFont(u8g2_font_6x10_tf);
+    u8g2.setCursor(startPos, positions[2]);
+    u8g2.print("Feels like ");
+
+    int currentPos = startPos + feelsLikeTempTextPos;
+    u8g2.setFont(u8g2_font_6x12_tf);
+    u8g2.setCursor(currentPos, positions[2]);
+    u8g2.print(feelsLikeTemp);
+
+    // Units symbol
+    currentPos += feelsLikeTempPos;
+    u8g2.drawCircle(currentPos + 2, positions[2] - 7, 1);
+    u8g2.drawStr(currentPos + 5, positions[2], "C");
 }
 
 void displayWeather()
