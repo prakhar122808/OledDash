c11a76ba39a124ba1188244b7e7ec0d8ec5a54e5
prakhar122808 <prakharshi2808@gmail.com>
2026-07-21 18:19:40 +0530

    Reorganised functions

--- src/time.cpp
+++ src/time.cpp
@@ -2,71 +2,8 @@
 
 #include "header.h"
 
-DateAndTime getDateAndTime()
-{
-
-    // Get the current date and time
-    struct tm timeInfo;
-    DateAndTime dateAndTime = {"", "", ""};
-
-    if (!getLocalTime(&timeInfo))
-    {
-        Serial.println("Failed to obtain time.");
-        return dateAndTime;
-    }
-    // Initialise to zero
-    char day[4] = {0};
-    char date[11] = {0};
-    char time[9] = {0};
-
-    strftime(day, sizeof(day), "%A", &timeInfo);
-    // strftime(date, 2, "%b", &timeInfo);
-    // converToMonthNumber(date);
-    strftime(date, sizeof(date), "%d/%m/%Y", &timeInfo);
-    strftime(time, sizeof(time), "%H:%M:%S", &timeInfo);
-
-    // Return value
-    std::snprintf(dateAndTime.day, sizeof(dateAndTime.day), "%s", day);
-    std::snprintf(dateAndTime.date, sizeof(dateAndTime.date), "%s", date);
-    std::snprintf(dateAndTime.time, sizeof(dateAndTime.time), "%s", time);
-    return dateAndTime;
-}
-
-// ---------------------------------Page 1----------------------------------
-void currentTime()
-{
-    u8g2.clearBuffer();
-    DateAndTime dateAndTime = getDateAndTime();
-
-    char *day = dateAndTime.day;
-    char *date = dateAndTime.date;
-    char *time = dateAndTime.time;
-
-    // Day and date
-    u8g2.setFont(u8g2_font_t0_11b_tf);
-    int dayWidth = u8g2.getStrWidth(day);
-
-    u8g2.setFont(u8g2_font_profont12_tf);
-    int dateWidth = u8g2.getStrWidth(date);
-
-    int spaceWidth = 4;
-    int totalDateWidth = dayWidth + spaceWidth + dateWidth;
-    int startPos = (128 - totalDateWidth) / 2;
-
-    u8g2.setFont(u8g2_font_t0_11b_tf);
-    u8g2.setCursor(startPos, positions[0]);
-    u8g2.print(day);
-
-    u8g2.setFont(u8g2_font_profont12_tf);
-    u8g2.setCursor(startPos + dayWidth + spaceWidth, positions[0]);
-    u8g2.print(date);
-
-    // Time
-    u8g2.setFont(u8g2_font_fub17_tn);
-    int timeWidth = u8g2.getStrWidth(time);
-    u8g2.setCursor((128 - timeWidth) / 2, 40);
-    u8g2.print(time);
-}
+DateAndTime getDateAndTime();
+void currentTime();
 
 void displayTime()
 {
@@ -106,4 +43,70 @@ void displayTime()
     prevDelay();
     selectDelay();
     u8g2.sendBuffer();
+}
+
+// ---------------------------------Page 1----------------------------------
+void currentTime()
+{
+    u8g2.clearBuffer();
+    DateAndTime dateAndTime = getDateAndTime();
+
+    char *day = dateAndTime.day;
+    char *date = dateAndTime.date;
+    char *time = dateAndTime.time;
+
+    // Day and date
+    u8g2.setFont(u8g2_font_t0_11b_tf);
+    int dayWidth = u8g2.getStrWidth(day);
+
+    u8g2.setFont(u8g2_font_profont12_tf);
+    int dateWidth = u8g2.getStrWidth(date);
+
+    int spaceWidth = 4;
+    int totalDateWidth = dayWidth + spaceWidth + dateWidth;
+    int startPos = (128 - totalDateWidth) / 2;
+
+    u8g2.setFont(u8g2_font_t0_11b_tf);
+    u8g2.setCursor(startPos, positions[0]);
+    u8g2.print(day);
+
+    u8g2.setFont(u8g2_font_profont12_tf);
+    u8g2.setCursor(startPos + dayWidth + spaceWidth, positions[0]);
+    u8g2.print(date);
+
+    // Time
+    u8g2.setFont(u8g2_font_fub17_tn);
+    int timeWidth = u8g2.getStrWidth(time);
+    u8g2.setCursor((128 - timeWidth) / 2, 40);
+    u8g2.print(time);
+}
+
+DateAndTime getDateAndTime()
+{
+
+    // Get the current date and time
+    struct tm timeInfo;
+    DateAndTime dateAndTime = {"", "", ""};
+
+    if (!getLocalTime(&timeInfo))
+    {
+        Serial.println("Failed to obtain time.");
+        return dateAndTime;
+    }
+    // Initialise to zero
+    char day[4] = {0};
+    char date[11] = {0};
+    char time[9] = {0};
+
+    strftime(day, sizeof(day), "%A", &timeInfo);
+    // strftime(date, 2, "%b", &timeInfo);
+    // converToMonthNumber(date);
+    strftime(date, sizeof(date), "%d/%m/%Y", &timeInfo);
+    strftime(time, sizeof(time), "%H:%M:%S", &timeInfo);
+
+    // Return value
+    std::snprintf(dateAndTime.day, sizeof(dateAndTime.day), "%s", day);
+    std::snprintf(dateAndTime.date, sizeof(dateAndTime.date), "%s", date);
+    std::snprintf(dateAndTime.time, sizeof(dateAndTime.time), "%s", time);
+    return dateAndTime;
 }
